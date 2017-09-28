d0b1971344b3fca620ba293781d831a67c45795f
Ryan Powell <rgpowell@calpoly.edu>
2017-09-28 15:54:16 -0700

    added notes; using standard rubiks notation

--- cube_classes.py
+++ cube_classes.py
@@ -1,18 +1,20 @@
 #All pieces of a Rubiks cube are either a Corner(3 colors), an Edge(2 colors), or a Middle(1 color)
-#Middles never move - white is always top, yellow bottom, orange/blue/red/green sides in that order
-#Colors are one of: t(op), u(nder), f(ront)/green, (bac)k/blue, l(eft)/orange, r(ight)/red
+#Middles never move - white is always up, yellow down, orange/blue/red/green sides in that order
+#Colors are one of: (u)p, (d)own, (f)ront, (b)ack, (l)eft, ()right
+
+#This needs to be WAY faster
 
 class Corner:
    #There are 8 corners. a corner is either on the top side or the bottom side
    # "left" and "up" means if you orient the cube s.t. the piece is in the upper left corner
-   def __init__(self, is_top, facing_color, left_color, up_color):
-      self.is_top = is_top #bool
+   def __init__(self, is_up, facing_color, left_color, up_color):
+      self.is_up = is_up #bool
       self.facing_color = facing_color
       self.left_color = left_color
       self.up_color = up_color
 
 class Edge:
-   #12 edges. edges can be on top, bottom, or middle layer
+   #12 edges. edges can be on up, down, or middle layer
    #if edge is on top or bottom, color1 is the facing color
    #otherwise, color1 is the counterclockwise color
    def __init__(self, layer, color1, color2):
@@ -20,30 +22,31 @@ class Edge:
       self.color1 = color1
       self.color2 = color2
 
-corner_set = [Corner(True, 't', 'l', 'b'),
-              Corner(True, 't', 'b', 'r'),
-              Corner(True, 't', 'f', 'l'),
-              Corner(True, 't', 'r', 'f'),
+#IDK if the "is_up" and "layer" parameters will be useful yet
+corner_set = [Corner(True, 'u', 'l', 'b'),#
+              Corner(True, 'u', 'b', 'r'),#
+              Corner(True, 'u', 'f', 'l'),
+              Corner(True, 'u', 'r', 'f'),
 
-              Corner(False, 'u', 'l', 'f'),
-              Corner(False, 'u', 'f', 'r'),
-              Corner(False, 'u', 'b', 'l'),
-              Corner(False, 'u', 'r', 'b')]
+              Corner(False, 'd', 'l', 'f'),
+              Corner(False, 'd', 'f', 'r'),
+              Corner(False, 'd', 'b', 'l'),#
+              Corner(False, 'd', 'r', 'b')]#
 
-edge_set = [Edge('top', 't', 'b'),
-            Edge('top', 't', 'l'),
-            Edge('top', 't', 'r'),
-            Edge('top', 't', 'f'),
+edge_set = [Edge('up', 'u', 'b'),#
+            Edge('up', 'u', 'l'),
+            Edge('up', 'u', 'r'),
+            Edge('up', 'u', 'f'),
             
-            Edge('bom', 'u', 'f'),  
-            Edge('bom', 'u', 'l'),  
-            Edge('bom', 'u', 'r'),  
-            Edge('bom', 'u', 'b'),  
+            Edge('down', 'd', 'f'),  
+            Edge('down', 'd', 'l'),  
+            Edge('down', 'd', 'r'),  
+            Edge('down', 'd', 'b'),# 
 
             Edge('mid', 'f', 'l'),
             Edge('mid', 'r', 'f'),
-            Edge('mid', 'l', 'b'),
-            Edge('mid', 'b', 'r')]
+            Edge('mid', 'l', 'b'),#
+            Edge('mid', 'b', 'r')]#
 
 class Cube:
    #Position matters. corner_arr = [topQ2, topQ1, topQ3, topQ4, underQ2, underQ1, underQ3, underQ4] (Q meaning quadrant)
@@ -51,16 +54,17 @@ class Cube:
    def __init__(self, corner_arr = corner_set, edge_arr = edge_set):
       self.corner_arr = corner_arr
       self.edge_arr = edge_arr
-
-   def get_top(self):
+   
+   #it would be cool if there was a better way to get the sides
+   def get_up(self):
       top_row = [self.corner_arr[0].facing_color, self.edge_arr[0].color1, self.corner_arr[1].facing_color]
-      mid_row = [self.edge_arr[1].color1, 't', self.edge_arr[2].color1]
+      mid_row = [self.edge_arr[1].color1, 'u', self.edge_arr[2].color1]
       bom_row = [self.corner_arr[2].facing_color, self.edge_arr[3].color1, self.corner_arr[3].facing_color]
       return [top_row, mid_row, bom_row]
 
-   def get_under(self):
+   def get_down(self):
       top_row = [self.corner_arr[4].facing_color, self.edge_arr[4].color1, self.corner_arr[5].facing_color]
-      mid_row = [self.edge_arr[5].color1, 'u', self.edge_arr[6].color1]
+      mid_row = [self.edge_arr[5].color1, 'd', self.edge_arr[6].color1]
       bom_row = [self.corner_arr[6].facing_color, self.edge_arr[7].color1, self.corner_arr[7].facing_color]
       return [top_row, mid_row, bom_row]
 
@@ -89,35 +93,78 @@ class Cube:
       return [top_row, mid_row, bom_row]
 
    def display(self):
-      t = self.get_top()
+      u = self.get_up()
       f = self.get_front()
-      u = self.get_under()
+      d = self.get_down()
       b = self.get_back()
       l = self.get_left()
       r = self.get_right()
 
+      #its like those paper cut out things that you turn into a box
       print()
-      print('       {:s}'.format(' '.join(t[0])))
-      print('       {:s}'.format(' '.join(t[1])))
-      print('       {:s}'.format(' '.join(t[2])))
+      print('       {:s}'.format(' '.join(u[0])))
+      print('       {:s}'.format(' '.join(u[1])))
+      print('       {:s}'.format(' '.join(u[2])))
       print()
       print('{:s}  {:s}  {:s}'.format(' '.join(l[0]), ' '.join(f[0]), ' '.join(r[0])))
       print('{:s}  {:s}  {:s}'.format(' '.join(l[1]), ' '.join(f[1]), ' '.join(r[1])))
       print('{:s}  {:s}  {:s}'.format(' '.join(l[2]), ' '.join(f[2]), ' '.join(r[2])))
       print()
-      print('       {:s}'.format(' '.join(u[0])))
-      print('       {:s}'.format(' '.join(u[1])))
-      print('       {:s}'.format(' '.join(u[2])))
+      print('       {:s}'.format(' '.join(d[0])))
+      print('       {:s}'.format(' '.join(d[1])))
+      print('       {:s}'.format(' '.join(d[2])))
       print()
       print('       {:s}'.format(' '.join(b[0])))
       print('       {:s}'.format(' '.join(b[1])))
       print('       {:s}'.format(' '.join(b[2])))
       print()
  
-   def clockwise_top(self):
+   def is_solved(self):
+      #could also just compare given cube to solved cube ake Cube()
+      #but that requires __eq__ method
+      u = self.get_up()
+      f = self.get_front()
+      d = self.get_down()
+      b = self.get_back()
+      l = self.get_left()
+      r = self.get_right()
+
+      sides = [u, f, d, b, l, r]
+      for side in sides:
+            for row in side:
+               if not(row[0] == row[1] == row[2]):
+                  return False
+      return True
+
+   #probably don't have to build new arrays to do this
+   def U(self):
       self.corner_arr = [self.corner_arr[2], self.corner_arr[0], self.corner_arr[3], self.corner_arr[1]] + self.corner_arr[4:]
       self.edge_arr = [self.edge_arr[1], self.edge_arr[3], self.edge_arr[0], self.edge_arr[2]] + self.edge_arr[4:]
 
-   def clockwise_under(self):
+   def D(self):
       self.corner_arr = self.corner_arr[:4] + [self.corner_arr[6], self.corner_arr[4], self.corner_arr[7], self.corner_arr[5]]
       self.edge_arr = self.edge_arr[:4] + [self.edge_arr[5], self.edge_arr[7], self.edge_arr[4], self.edge_arr[6]] + self.edge_arr[8:]
+
+   #Everything after U and D is pretty gross
+   def F(self):
+      [c0, c1, c2, c3] = self.corner_arr[2:6]
+      self.corner_arr = self.corner_arr[:2] + [Corner(True, c2.left_color, c2.up_color, c2.facing_color), 
+                                               Corner(True, c0.up_color, c0.facing_color, c0.left_color),
+                                               Corner(False, c3.up_color, c3.facing_color, c3.left_color),
+                                               Corner(False, c1.left_color, c1.up_color, c1.facing_color)] + self.corner_arr[6:] 
+      e0 = self.edge_arr[3]
+      e1 = self.edge_arr[4]
+      e2 = self.edge_arr[8]
+      e3 = self.edge_arr[9]
+      self.edge_arr = self.edge_arr[:3] + [Edge('top', e2.color2, e2.color1), e3] + self.edge_arr[5:8] + [Edge('mid', e1.color2, e1.color1), Edge('mid', e0.color1, e0.color2)] + self.edge_arr[10:]
+
+   def B(self):
+      [c0, c1] = self.corner_arr[:2]
+      [c2, c3] = self.corner_arr[6:]
+      self.corner_arr = [] + self.corner_arr[2:6] + []
+      self.edge_arr = []
+
+c = Cube()
+for i in range(1000000):
+   c.F()
+c.display()
---
+++ notes.md
@@ -0,0 +1,19 @@
+Phase 1: Interface
+   Displaying a 3D cube is hard
+   Turning is complicated
+   Make sure it works
+
+Phase 2: Optimize that interface
+   It needs to be super fast so you can run it a billion times
+
+Phase 3: Design a solver
+   Use preexisting algorithms
+
+Phase 4: Make the solver AI
+   Neural net?
+   Whats the cost function
+
+Phase 5: Make it pretty
+   Add some front end stuff
+   Way to enter cube configuration
+   3D?
